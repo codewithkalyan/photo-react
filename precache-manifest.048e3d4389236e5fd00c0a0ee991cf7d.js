9748553e584e42a6e853c5672800d0572623d9df
codewithkalyan <kishorekalyan97@gmail.com>
2024-03-02 20:25:49 +0530

    Updates

---
+++ precache-manifest.048e3d4389236e5fd00c0a0ee991cf7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efd0ef7f23fd9be7c4d17c09fc294da",
+    "url": "/photo-react/index.html"
+  },
+  {
+    "revision": "2974f510692820d216e4",
+    "url": "/photo-react/static/css/main.e4a28c8b.chunk.css"
+  },
+  {
+    "revision": "245ff4c6bf21eb7194fa",
+    "url": "/photo-react/static/js/2.45d4a8a0.chunk.js"
+  },
+  {
+    "revision": "e44fc52883c1d616df3fb6cbdfba7835",
+    "url": "/photo-react/static/js/2.45d4a8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9772e5332acf574fed5b",
+    "url": "/photo-react/static/js/3.9931d8b3.chunk.js"
+  },
+  {
+    "revision": "2974f510692820d216e4",
+    "url": "/photo-react/static/js/main.c8bb6f6a.chunk.js"
+  },
+  {
+    "revision": "11018f291b03612d34fc",
+    "url": "/photo-react/static/js/runtime-main.72485ba6.js"
+  }
+]);
